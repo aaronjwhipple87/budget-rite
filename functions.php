ebfb815c7ec632ea49762941f1fa6b9b8933d521
Aaron Whipple <ohhellwhipple@gmail.com>
2021-01-05 12:09:02 -0700

    fixed con error

--- functions.php
+++ functions.php
@@ -21,7 +21,7 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 
 if (mysqli_connect_errno() ) {
